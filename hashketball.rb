c5c51598f168f3cfd4c02789b772602718f8474b
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-10-05 23:18:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,103 +127,98 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
+def num_points_scored (player_name)
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :players
-        details.each do |stats|
-          if stats[:player_name] == player_name
-          return stats[:points]
+    team.each do |team_details, details|
+      if team_details == :players
+        details.each do |player_stats|
+          if player_stats[:player_name] == player_name
+            return player_stats[:points]
           end
-        end   
-        end   
-      end   
+        end
+      end
+    end
   end
+  
 end
 
-def shoe_size(player_name)
-  game_hash.each do |location,team|
-    team.each do |team_info, details|
-      if team_info == :players
-        details.each do |stats|
-          if stats[:player_name] == player_name
-            return stats[:shoe]
-          end
-        end   
-      end  
-    end   
+def shoe_size (player_name)
+  game_hash.each do |location, team|
+    team.each do |team_details, details|
+     if team_details == :players
+       details.each do |player_stats|
+         if player_stats[:player_name] == player_name
+           return player_stats[:shoe]
+         end
+       end
+     end
+    end
   end
+  
 end
 
-def team_colors(team_name)
+
+
+def team_colors (team_name)
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-        if details == team_name
-          return team[:colors]
-        end   
-    end  
-  end  
+    if team[:team_name] == team_name
+      return team[:colors]
+    end
+  end
 end
 
 def team_names
-teams = []
+  teams = []
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :team_name
-        teams << team[:team_name]
-      end
-    end 
+    teams << team[:team_name]
   end
   teams
 end
 
 def player_numbers (team_name)
-jersey_numbers = []
+  jersey_numbers = []
   game_hash.each do |location, team|
-    if team[:team_name] == team_name 
-      team.each do |team_info, details|
-        if team_info == :players
-          details.each do |stats|
-                jersey_numbers << stats[:number]
-          end 
-        end
+    if team[:team_name] == team_name
+      team[:players].each do |player_stats|
+      jersey_numbers << player_stats[:number]
       end
     end
-  end   
+  end
   jersey_numbers
-end
+end 
 
 def player_stats (player_name)
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :players
-        details.each do |stats|
-            if stats[:player_name] == player_name
-              return stats
-            end
-          end   
-        end 
-    end   
-  end  
-end   
+    team.each do |team_details, details|
+      if team_details == :players
+        details.each do |player_stats|
+          if player_stats[:player_name] == player_name
+            return player_stats
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
 
 
 def big_shoe_rebounds
-  largest_shoe = nil
-  rebounds = nil
+large_size = nil 
+rebounds = nil  
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-       
-      if team_info == :players
-        details.each do |stats|
-          
-          if largest_shoe == nil || largest_shoe < stats[:shoe]
-            largest_shoe = stats[:shoe]
-            rebounds = stats[:rebounds]
-          end 
-        end  
-      end 
-    end 
+    team.each do |team_details, details|
+     if team_details == :players
+       details.each do |player_stats|
+        if large_size == nil || large_size < player_stats[:shoe]
+          large_size = player_stats[:shoe]
+          rebounds = player_stats[:rebounds]
+        end
+       end
+      end
+    end
   end
   rebounds
 end
@@ -232,92 +227,98 @@ def most_points_scored
 points = nil
 mvp = nil
   game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :players
-        details.each do |stats|
-          
-          if points == nil || points < stats[:points]
-            points = stats[:points]
-            mvp = stats[:player_name]
-          end
-        end 
-      end 
+    team.each do |team_details, details|
+     if team_details == :players
+       details.each do |player_stats|
+        if points == nil || points < player_stats[:points]
+          points = player_stats[:points]
+          mvp = player_stats[:player_name]
+        end
+      end
     end
   end
-  mvp
 end
-
+mvp
+end
+ 
 def winning_team
-  home_array = []
-  away_array = []
-  home_points = nil
-  away_points = nil
-  
- game_hash.each do |location, team|
-    if location == :home
-      team.each do |team_info, details|
-        if team_info == :players
-          details.each do |stats|
-            home_array << stats[:points]
-            home_points = home_array.reduce (0) {|total, num| total + num}
-          end   
-        end   
+home_points = 0
+home_team = nil
+away_points = 0
+away_team = nil
+
+  game_hash.each do |location, team|
+    if location == :home 
+    home_team = team[:team_name]
+    team.each do |team_details, details|
+      if team_details == :players
+        details.each do |player_stats|
+         home_points += player_stats[:points]
+        end
       end
-    elsif location == :away
-       team.each do |team_info, details|
-        if team_info == :players
-          details.each do |stats|
-            away_array << stats[:points]
-            away_points = away_array.reduce (0) {|total, num| total + num}
+    end
+  else
+    away_team = team[:team_name]
+    team.each do |team_details, details|
+      if team_details == :players
+        details.each do |player_stats|
+         away_points += player_stats[:points]
+        end
+      end
+    end
+  end 
+end 
+  if home_points > away_points
+    "#{home_team} Wins!"
+  else
+    "#{away_team} Wins!"
+  end
+end 
+
+def player_with_longest_name
+  longest_name = nil
+  game_hash.each do |location, team|
+    team.each do |team_details, details|
+      if team_details == :players
+        details.each do |player_stats|
+          if longest_name == nil || longest_name.length < player_stats[:player_name].length 
+            
+            longest_name = player_stats[:player_name]
           end
         end
-      end 
+      end
     end
   end
-   if home_points > away_points
-    return "Home Wins!"
-    elsif home_points < away_points
-    return "Away Wins!"
-    end
- 
- end 
- 
- def player_with_longest_name
-   longest_name = nil
-   game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :players
-        details.each do |stats|
-          if longest_name == nil || longest_name.length < stats[:player_name].length 
-            longest_name = stats[:player_name]
-          end
-        end 
-      end
-    end
-   end
-   longest_name
- end
+longest_name
+end
 
- def long_name_steals_a_ton? (player_with_longest_name)
-  steals = nil 
-  steals_array = []
-    game_hash.each do |location, team|
-    team.each do |team_info, details|
-      if team_info == :players
-       
-        details.each do |stats|
-         
-           if stats[:player_name] == player_with_longest_name
-            steals = stats[:steals]
-          end 
-          
-          steals_array << stats[:steals]
-      end
+def long_name_steals_a_ton? (player_with_longest_name)
+  steals = 0
+  all_steals = []
+  game_hash.each do |location, team|
+    team.each do |team_details, details|
+     if team_details == :players
+       details.each do |player_stats|
+        
+        if player_stats[:player_name] == player_with_longest_name
+           steals = player_stats[:steals]
+        end 
+        
+        all_steals << player_stats[:steals]
+        
+       end
       end
     end
-    end
-  steals_array.max == steals
- end 
- 
- 
- 
+  end
+all_steals.max == steals
+end
+
+
+
+
+
+
+
+
+
+
